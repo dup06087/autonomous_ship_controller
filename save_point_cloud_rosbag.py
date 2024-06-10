ce856b02226eef32e57e4658a81bc29b6965593a
dup06087 <dup06087@naver.com>
2024-06-10 16:06:26 +0900

    added assembled point cloud logger

--- save_point_cloud_rosbag.py
+++ save_point_cloud_rosbag.py
@@ -7,18 +7,26 @@ from datetime import datetime
 import os
 
 class PointCloud2Bag:
-    def __init__(self, topic, folder_path):
+    def __init__(self, topic):
         self.topic = topic
         self.start_time = datetime.now()  # 시작 시간 기록
-        self.folder_path = folder_path
-        self.ensure_folder_exists(folder_path)
-        self.bag_file_name = self.generate_bag_filename(self.start_time, folder_path)
+        self.folder_path = self.create_log_folder(self.start_time)
+        self.bag_file_name = self.generate_bag_filename(self.start_time, self.folder_path)
         self.bag = rosbag.Bag(self.bag_file_name, 'w')
         self.subscriber = rospy.Subscriber(topic, PointCloud2, self.callback)
 
-    def ensure_folder_exists(self, folder_path):
+    def create_log_folder(self, start_time):
+        # Create folder path based on the current year, month-date, and time
+        year = start_time.strftime("%Y")
+        month_date = start_time.strftime("%m-%d")
+        time = start_time.strftime("%H%M")
+        folder_path = os.path.join("log_pc", year, month_date, time)
+        
+        # Ensure the folder exists
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
+        
+        return folder_path
 
     def generate_bag_filename(self, start_time, folder_path):
         # 파일명 형식: pc_data_년_월_일_시간.bag
@@ -36,9 +44,8 @@ class PointCloud2Bag:
 if __name__ == '__main__':
     rospy.init_node('pointcloud_to_rosbag', anonymous=True)
     topic_name = '/velodyne_points'  # VLP-16 포인트클라우드 데이터가 발행되는 토픽 이름
-    folder_path = 'pointcloud_data'  # 저장할 폴더 이름
 
-    pc2_bag = PointCloud2Bag(topic_name, folder_path)
+    pc2_bag = PointCloud2Bag(topic_name)
     
     rospy.on_shutdown(pc2_bag.close_bag)  # 노드 종료 시 rosbag 파일 닫기
 
